c60403a233b87f2d96372f208b492112dc97ac1e
braferraz <braianfs2009@gmail.com>
2020-10-30 23:29:14 -0300

    Form para inserção de novos posts

--- src/main/java/com/spring/blog/controller/BlogController.java
+++ src/main/java/com/spring/blog/controller/BlogController.java
@@ -1,13 +1,18 @@
 package com.spring.blog.controller;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.spring.blog.model.Post;
 import com.spring.blog.service.BlogService;
@@ -32,5 +37,20 @@ public class BlogController {
 		Post post = blogService.findById(id); 
 		mv.addObject("post", post);
 		return mv;
+	}
+	
+	@RequestMapping(value = "/newpost", method = RequestMethod.GET)
+	public String getPostForm() {
+		return "postForm";
+	}
+	
+	@RequestMapping(value = "/newpost", method = RequestMethod.POST)
+	public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			return "redirect:/newpost";
+		}
+		post.setData(LocalDate.now());
+		blogService.save(post);
+		return "redirect:/posts";
 	}
 }
